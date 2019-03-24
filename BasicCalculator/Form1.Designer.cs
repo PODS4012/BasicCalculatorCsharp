eb542d2fa0ef6f5bc2ffad8f49d03d2730ea06b3
PODS4012 <pods4012@gmail.com>
2019-03-24 15:06:51 +0100

    added methods to the button - equation

--- BasicCalculator/Form1.Designer.cs
+++ BasicCalculator/Form1.Designer.cs
@@ -32,7 +32,6 @@
             this.calcResultText = new System.Windows.Forms.Label();
             this.buttonsPanel = new System.Windows.Forms.TableLayoutPanel();
             this.btnCE = new System.Windows.Forms.Button();
-            this.btnC = new System.Windows.Forms.Button();
             this.btnBack = new System.Windows.Forms.Button();
             this.btn7 = new System.Windows.Forms.Button();
             this.btn8 = new System.Windows.Forms.Button();
@@ -47,7 +46,6 @@
             this.btn2 = new System.Windows.Forms.Button();
             this.btn3 = new System.Windows.Forms.Button();
             this.btnPlus = new System.Windows.Forms.Button();
-            this.button16 = new System.Windows.Forms.Button();
             this.btn0 = new System.Windows.Forms.Button();
             this.button18 = new System.Windows.Forms.Button();
             this.btnEqual = new System.Windows.Forms.Button();
@@ -60,7 +58,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.UserInputText.Location = new System.Drawing.Point(12, 22);
             this.UserInputText.Name = "UserInputText";
-            this.UserInputText.Size = new System.Drawing.Size(210, 20);
+            this.UserInputText.Size = new System.Drawing.Size(415, 20);
             this.UserInputText.TabIndex = 0;
             // 
             // calcResultText
@@ -69,7 +67,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.calcResultText.Location = new System.Drawing.Point(13, 49);
             this.calcResultText.Name = "calcResultText";
-            this.calcResultText.Size = new System.Drawing.Size(209, 17);
+            this.calcResultText.Size = new System.Drawing.Size(414, 17);
             this.calcResultText.TabIndex = 1;
             this.calcResultText.Text = "Enter an equation and press enter or =";
             // 
@@ -84,7 +82,6 @@
             this.buttonsPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.buttonsPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.buttonsPanel.Controls.Add(this.btnCE, 0, 0);
-            this.buttonsPanel.Controls.Add(this.btnC, 1, 0);
             this.buttonsPanel.Controls.Add(this.btnBack, 2, 0);
             this.buttonsPanel.Controls.Add(this.btn7, 0, 1);
             this.buttonsPanel.Controls.Add(this.btn8, 1, 1);
@@ -99,7 +96,6 @@
             this.buttonsPanel.Controls.Add(this.btn2, 1, 3);
             this.buttonsPanel.Controls.Add(this.btn3, 2, 3);
             this.buttonsPanel.Controls.Add(this.btnPlus, 3, 3);
-            this.buttonsPanel.Controls.Add(this.button16, 0, 4);
             this.buttonsPanel.Controls.Add(this.btn0, 1, 4);
             this.buttonsPanel.Controls.Add(this.button18, 2, 4);
             this.buttonsPanel.Controls.Add(this.btnEqual, 3, 4);
@@ -111,215 +107,216 @@
             this.buttonsPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.buttonsPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.buttonsPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.buttonsPanel.Size = new System.Drawing.Size(210, 200);
+            this.buttonsPanel.Size = new System.Drawing.Size(415, 357);
             this.buttonsPanel.TabIndex = 2;
             // 
             // btnCE
             // 
+            this.btnCE.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnCE.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnCE.Location = new System.Drawing.Point(3, 3);
             this.btnCE.Name = "btnCE";
-            this.btnCE.Size = new System.Drawing.Size(46, 34);
+            this.btnCE.Size = new System.Drawing.Size(97, 65);
             this.btnCE.TabIndex = 0;
             this.btnCE.Text = "CE";
             this.btnCE.UseVisualStyleBackColor = true;
-            // 
-            // btnC
-            // 
-            this.btnC.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnC.Location = new System.Drawing.Point(55, 3);
-            this.btnC.Name = "btnC";
-            this.btnC.Size = new System.Drawing.Size(46, 34);
-            this.btnC.TabIndex = 1;
-            this.btnC.Text = "C";
-            this.btnC.UseVisualStyleBackColor = true;
+            this.btnCE.Click += new System.EventHandler(this.btnCE_Click);
             // 
             // btnBack
             // 
             this.btnBack.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnBack.Location = new System.Drawing.Point(107, 3);
+            this.btnBack.Location = new System.Drawing.Point(209, 3);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(46, 34);
+            this.btnBack.Size = new System.Drawing.Size(97, 65);
             this.btnBack.TabIndex = 2;
             this.btnBack.TabStop = false;
             this.btnBack.Text = "◄";
             this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // btn7
             // 
             this.btn7.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn7.Location = new System.Drawing.Point(3, 43);
+            this.btn7.Location = new System.Drawing.Point(3, 74);
             this.btn7.Name = "btn7";
-            this.btn7.Size = new System.Drawing.Size(46, 34);
+            this.btn7.Size = new System.Drawing.Size(97, 65);
             this.btn7.TabIndex = 3;
             this.btn7.Text = "7";
             this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
             // 
             // btn8
             // 
             this.btn8.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn8.Location = new System.Drawing.Point(55, 43);
+            this.btn8.Location = new System.Drawing.Point(106, 74);
             this.btn8.Name = "btn8";
-            this.btn8.Size = new System.Drawing.Size(46, 34);
+            this.btn8.Size = new System.Drawing.Size(97, 65);
             this.btn8.TabIndex = 4;
             this.btn8.Text = "8";
             this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
             // 
             // btn9
             // 
             this.btn9.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn9.Location = new System.Drawing.Point(107, 43);
+            this.btn9.Location = new System.Drawing.Point(209, 74);
             this.btn9.Name = "btn9";
-            this.btn9.Size = new System.Drawing.Size(46, 34);
+            this.btn9.Size = new System.Drawing.Size(97, 65);
             this.btn9.TabIndex = 5;
             this.btn9.Text = "9";
             this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
             // 
             // btnDevide
             // 
             this.btnDevide.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnDevide.Location = new System.Drawing.Point(159, 3);
+            this.btnDevide.Location = new System.Drawing.Point(312, 3);
             this.btnDevide.Name = "btnDevide";
-            this.btnDevide.Size = new System.Drawing.Size(48, 34);
+            this.btnDevide.Size = new System.Drawing.Size(100, 65);
             this.btnDevide.TabIndex = 6;
             this.btnDevide.Text = "/";
             this.btnDevide.UseVisualStyleBackColor = true;
+            this.btnDevide.Click += new System.EventHandler(this.btnDevide_Click);
             // 
             // btnx
             // 
             this.btnx.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnx.Location = new System.Drawing.Point(159, 43);
+            this.btnx.Location = new System.Drawing.Point(312, 74);
             this.btnx.Name = "btnx";
-            this.btnx.Size = new System.Drawing.Size(48, 34);
+            this.btnx.Size = new System.Drawing.Size(100, 65);
             this.btnx.TabIndex = 7;
             this.btnx.Text = "x";
             this.btnx.UseVisualStyleBackColor = true;
-            this.btnx.Click += new System.EventHandler(this.button7_Click);
+            this.btnx.Click += new System.EventHandler(this.btnx_Click);
             // 
             // btn4
             // 
             this.btn4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn4.Location = new System.Drawing.Point(3, 83);
+            this.btn4.Location = new System.Drawing.Point(3, 145);
             this.btn4.Name = "btn4";
-            this.btn4.Size = new System.Drawing.Size(46, 34);
+            this.btn4.Size = new System.Drawing.Size(97, 65);
             this.btn4.TabIndex = 8;
             this.btn4.Text = "4";
             this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
             // 
             // btn5
             // 
             this.btn5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn5.Location = new System.Drawing.Point(55, 83);
+            this.btn5.Location = new System.Drawing.Point(106, 145);
             this.btn5.Name = "btn5";
-            this.btn5.Size = new System.Drawing.Size(46, 34);
+            this.btn5.Size = new System.Drawing.Size(97, 65);
             this.btn5.TabIndex = 9;
             this.btn5.Text = "5";
             this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
             // 
             // btn6
             // 
             this.btn6.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn6.Location = new System.Drawing.Point(107, 83);
+            this.btn6.Location = new System.Drawing.Point(209, 145);
             this.btn6.Name = "btn6";
-            this.btn6.Size = new System.Drawing.Size(46, 34);
+            this.btn6.Size = new System.Drawing.Size(97, 65);
             this.btn6.TabIndex = 10;
             this.btn6.Text = "6";
             this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
             // 
             // btnMinus
             // 
             this.btnMinus.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnMinus.Location = new System.Drawing.Point(159, 83);
+            this.btnMinus.Location = new System.Drawing.Point(312, 145);
             this.btnMinus.Name = "btnMinus";
-            this.btnMinus.Size = new System.Drawing.Size(48, 34);
+            this.btnMinus.Size = new System.Drawing.Size(100, 65);
             this.btnMinus.TabIndex = 11;
             this.btnMinus.Text = "-";
             this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
             // 
             // btn1
             // 
             this.btn1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn1.Location = new System.Drawing.Point(3, 123);
+            this.btn1.Location = new System.Drawing.Point(3, 216);
             this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(46, 34);
+            this.btn1.Size = new System.Drawing.Size(97, 65);
             this.btn1.TabIndex = 12;
             this.btn1.Text = "1";
             this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
             // 
             // btn2
             // 
             this.btn2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn2.Location = new System.Drawing.Point(55, 123);
+            this.btn2.Location = new System.Drawing.Point(106, 216);
             this.btn2.Name = "btn2";
-            this.btn2.Size = new System.Drawing.Size(46, 34);
+            this.btn2.Size = new System.Drawing.Size(97, 65);
             this.btn2.TabIndex = 13;
             this.btn2.Text = "2";
             this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
             // 
             // btn3
             // 
             this.btn3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn3.Location = new System.Drawing.Point(107, 123);
+            this.btn3.Location = new System.Drawing.Point(209, 216);
             this.btn3.Name = "btn3";
-            this.btn3.Size = new System.Drawing.Size(46, 34);
+            this.btn3.Size = new System.Drawing.Size(97, 65);
             this.btn3.TabIndex = 14;
             this.btn3.Text = "3";
             this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
             // 
             // btnPlus
             // 
             this.btnPlus.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnPlus.Location = new System.Drawing.Point(159, 123);
+            this.btnPlus.Location = new System.Drawing.Point(312, 216);
             this.btnPlus.Name = "btnPlus";
-            this.btnPlus.Size = new System.Drawing.Size(48, 34);
+            this.btnPlus.Size = new System.Drawing.Size(100, 65);
             this.btnPlus.TabIndex = 15;
             this.btnPlus.Text = "+";
             this.btnPlus.UseVisualStyleBackColor = true;
-            // 
-            // button16
-            // 
-            this.button16.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button16.Location = new System.Drawing.Point(3, 163);
-            this.button16.Name = "button16";
-            this.button16.Size = new System.Drawing.Size(46, 34);
-            this.button16.TabIndex = 16;
-            this.button16.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
             // 
             // btn0
             // 
             this.btn0.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btn0.Location = new System.Drawing.Point(55, 163);
+            this.btn0.Location = new System.Drawing.Point(106, 287);
             this.btn0.Name = "btn0";
-            this.btn0.Size = new System.Drawing.Size(46, 34);
+            this.btn0.Size = new System.Drawing.Size(97, 67);
             this.btn0.TabIndex = 17;
             this.btn0.Text = "0";
             this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
             // 
             // button18
             // 
             this.button18.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button18.Location = new System.Drawing.Point(107, 163);
+            this.button18.Location = new System.Drawing.Point(209, 287);
             this.button18.Name = "button18";
-            this.button18.Size = new System.Drawing.Size(46, 34);
+            this.button18.Size = new System.Drawing.Size(97, 67);
             this.button18.TabIndex = 18;
             this.button18.Text = ",";
             this.button18.UseVisualStyleBackColor = true;
+            this.button18.Click += new System.EventHandler(this.btnDot);
             // 
             // btnEqual
             // 
             this.btnEqual.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnEqual.Location = new System.Drawing.Point(159, 163);
+            this.btnEqual.Location = new System.Drawing.Point(312, 287);
             this.btnEqual.Name = "btnEqual";
-            this.btnEqual.Size = new System.Drawing.Size(48, 34);
+            this.btnEqual.Size = new System.Drawing.Size(100, 67);
             this.btnEqual.TabIndex = 19;
             this.btnEqual.Text = "=";
             this.btnEqual.UseVisualStyleBackColor = true;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click);
             // 
             // Form1
             // 
+            this.AcceptButton = this.btnEqual;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(234, 281);
+            this.CancelButton = this.btnCE;
+            this.ClientSize = new System.Drawing.Size(439, 438);
             this.Controls.Add(this.buttonsPanel);
             this.Controls.Add(this.calcResultText);
             this.Controls.Add(this.UserInputText);
@@ -340,7 +337,6 @@
         private System.Windows.Forms.Label calcResultText;
         private System.Windows.Forms.TableLayoutPanel buttonsPanel;
         private System.Windows.Forms.Button btnCE;
-        private System.Windows.Forms.Button btnC;
         private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.Button btn7;
         private System.Windows.Forms.Button btn8;
@@ -355,7 +351,6 @@
         private System.Windows.Forms.Button btn2;
         private System.Windows.Forms.Button btn3;
         private System.Windows.Forms.Button btnPlus;
-        private System.Windows.Forms.Button button16;
         private System.Windows.Forms.Button btn0;
         private System.Windows.Forms.Button button18;
         private System.Windows.Forms.Button btnEqual;
--- BasicCalculator/Form1.cs
+++ BasicCalculator/Form1.cs
@@ -1,25 +1,262 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BasicCalculator
 {
+    /// <summary>
+    /// A basic calculator
+    /// </summary>
     public partial class Form1 : Form
     {
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button7_Click(object sender, EventArgs e)
+        #endregion
+
+        #region Clearing Methods
+
+        /// <summary>
+        /// Clears the user input text
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event arguments</param>
+        private void btnCE_Click(object sender, EventArgs e)
+        {
+            // Clears the text from the user input box
+            this.UserInputText.Text = string.Empty;
+
+            // Focus the user input text box
+            FocusInputText();
+        }
+
+        /// <summary>
+        /// Deletes the first character in front of the cursor
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event argument</param>
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            // Clears the text from the user input text box
+            DeleteTextValue();
+
+            // Focus the user input text box
+            FocusInputText();
+        }
+
+        #endregion
+
+        #region Operators Methods
+
+        private void btnDevide_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("/");
+
+            // Focus the user input text box
+            FocusInputText();
+        }
+
+        private void btnx_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("*");
+
+            // Focus the user input text box
+            FocusInputText();
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("-");
+
+            // Focus the user input text box
+            FocusInputText();
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("+");
+
+            // Focus the user input text box
+            FocusInputText();
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            CalculateEquation();
+
+            // Focus the user input text box
+            FocusInputText();
+        }
+
+        
+
+        #endregion
+
+        #region Number Methods
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("0");
+
+            // Focus the user input text box
+            FocusInputText();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("1");
+
+            // Focus the user input text box
+            FocusInputText();
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("2");
+
+            // Focus the user input text box
+            FocusInputText();
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("3");
+
+            // Focus the user input text box
+            FocusInputText();
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("4");
+
+            // Focus the user input text box
+            FocusInputText();
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("5");
+
+            // Focus the user input text box
+            FocusInputText();
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("6");
+
+            // Focus the user input text box
+            FocusInputText();
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("7");
+
+            // Focus the user input text box
+            FocusInputText();
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("8");
+
+            // Focus the user input text box
+            FocusInputText();
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            InsertTextValue("9");
+
+            // Focus the user input text box
+            FocusInputText();
+        }
+
+        private void btnDot(object sender, EventArgs e)
+        {
+            InsertTextValue(",");
+
+            // Focus the user input text box
+            FocusInputText();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Calculate the given equation and outputs the answer to the user label
+        /// </summary>
+        private void CalculateEquation()
         {
 
+            // TODO: finnish
+
+            // Focus the user input text box
+            FocusInputText();
         }
+
+        #region Private Helpers
+
+        /// <summary>
+        /// Focuses the user input box
+        /// </summary>
+        private void FocusInputText()
+        {
+            this.UserInputText.Focus();
+        }
+
+        /// <summary>
+        /// Inserts the given text into the user input text box
+        /// </summary>
+        /// <param name="value">The value to insert</param>
+        private void InsertTextValue(string value)
+        {
+            // Remember selection start
+            var selectionStrart = this.UserInputText.SelectionStart;
+
+            // Set new text
+            this.UserInputText.Text = this.UserInputText.Text.Insert(this.UserInputText.SelectionStart, value);
+
+            // Restore the selection start
+            this.UserInputText.SelectionStart = selectionStrart + value.Length;
+
+            // Set selection length to zero
+            this.UserInputText.SelectionLength = 0;
+        }
+
+        /// <summary>
+        /// Delete the character to the right of the selection start of the user input text box
+        /// </summary>
+        private void DeleteTextValue()
+        {
+            // If we don't have a value to delete, return
+            if (this.UserInputText.Text.Length < this.UserInputText.SelectionStart + 1)
+                return;
+
+            // Remember selection start
+            var selectionStrart = this.UserInputText.SelectionStart;
+
+            // Delete the character to the right of the selection
+            this.UserInputText.Text = this.UserInputText.Text.Remove(this.UserInputText.SelectionStart, 1);
+
+
+            // Restore the selection start
+            this.UserInputText.SelectionStart = selectionStrart;
+
+            // Set selection length to zero
+            this.UserInputText.SelectionLength = 0;
+        }
+
+
+
+        #endregion
+
     }
 }
